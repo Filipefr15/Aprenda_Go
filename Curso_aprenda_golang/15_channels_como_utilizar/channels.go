665f9c277e81eb78c4d2dcbe7634676cd483400a
Filipefr15 <filipe-ferreira-1@hotmail.com>
2024-06-28 12:55:04 -0400

    quase fim do curso

---
+++ Curso_aprenda_golang/15_channels_como_utilizar/channels.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func numeros(done chan<- bool) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d ", i)
+		time.Sleep(time.Millisecond * 150)
+	}
+	done <- true
+}
+
+func letras(done chan<- bool) {
+	for l := 'a'; l < 'j'; l++ {
+		fmt.Printf("%q ", l) //o certo é utilizar %c, char...
+		time.Sleep(time.Millisecond * 230)
+	}
+	done <- true
+}
+
+func main() {
+	cn := make(chan bool)
+	cl := make(chan bool)
+
+	go numeros(cn)
+	go letras(cl)
+
+	<-cn
+	<-cl
+
+	fmt.Println("Fim da execução!")
+}
